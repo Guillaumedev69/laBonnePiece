16c4480043f951b7e2f356ab2f890c574ea12b1d
Guillaume Destree <devstree.guillaume@gmail.com>
2024-02-13 14:57:34 +0100

    Recuperation des avis + ajout note sur 5

--- avis.js
+++ avis.js
@@ -10,11 +10,32 @@ export function ajoutListenerAvis() {
             const pieceElement = event.target.parentElement;
             const avisElement = document.createElement("p");
             for (let i = 0; i < avis.length; i++) {
-                avisElement.innerHTML += `<b>${avis[i].utilisateur}:</b> ${avis[i].commentaire}
+                avisElement.innerHTML += `<b>${avis[i].utilisateur}:</b> ${avis[i].commentaire} ${avis[i].nbEtoiles}
                 <br>`;
             }
             pieceElement.appendChild(avisElement)
         });
         
     }
+}
+
+export function ajoutListenerEnvoyerAvis() {
+    const formulaireAvis = document.querySelector(".formulaire-avis");
+    formulaireAvis.addEventListener("submit",function (event) {
+        // Désactivation du comportement par défaut du navigateur
+        event.preventDefault()
+        const avis = {
+            pieceId: parseInt(event.target.querySelector("[name=piece-id").value),
+            utilisateur: event.target.querySelector("[name=utilisateur]").value,
+            commentaire: event.target.querySelector("[name=commentaire]").value,
+            nbEtoiles: parseInt(event.target.querySelector("[name=nbEtoiles]").value)
+        };
+        const chargeUtile = JSON.stringify(avis);
+        fetch("http://localhost:8081/avis", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: chargeUtile
+        });
+    });
+    
 }
